d7309356227683fdc64ba7c9f4529b4972836384
BanyanS <162513125+Johnjacob12345@users.noreply.github.com>
2026-01-17 15:50:17 -0800

    first commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,18 @@
+package frc.robot;
+
+public final class Constants {
+    
+
+    //change these values to adjust the speed of the robot's movement
+     public static final class DriveConstants {
+         public static final double FORWARD_REVERSED_SPEED = 0.5;
+         public static final double LEFT_RIGHT_SPEED = 0.5;
+         public static final double ROTATION_SPEED = 0.5;
+        }
+    
+  
+    //change JOYSTICK_DEADZONE to adjust the deadzone threshold on joystick inputs
+    public static final class DeadzoneConstants {
+        public static final double JOYSTICK_DEADZONE = 0.2; 
+    }
+}
